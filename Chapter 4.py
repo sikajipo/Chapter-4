09dc23e73f0abb9bd8f0945544d8c19cebffb0d5
sikajipo <30323306+sikajipo@users.noreply.github.com>
2017-08-01 12:10:37 -0400

    Add files via upload

---
+++ Chapter 4.py
@@ -0,0 +1,18 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> spam = ['cat', 'bat', 'rat', 'elephant']
+>>> spam[0]
+'cat'
+>>> spam[1]
+'bat'
+>>> spam[2]
+'rat'
+>>> spam[3]
+'elephant'
+>>> ['cat', 'bat', 'rat', 'elephant'][3]
+'elephant'
+>>> 'Hello ' + spam[0]
+'Hello cat'
+>>> 'The ' + spam[1] + ' ate the ' + spam[0] + '.'
+'The bat ate the cat.'
+>>> 
